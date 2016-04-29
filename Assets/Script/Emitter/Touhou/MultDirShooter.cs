e47ce7fe111d4812e46e1943c778a861545d2d85
sdqq1234 <zhangsdqq1234@foxmail.com>
2016-04-29 15:44:11 +0900

    fix mulDirShooter

--- Assets/Script/Emitter/Touhou/MultDirShooter.cs
+++ Assets/Script/Emitter/Touhou/MultDirShooter.cs
@@ -5,79 +5,136 @@ using System.Collections.Generic;
 //多方向弹幕
 public class MultDirShooter : EmitterBase
 {
-
-    ////Vector2[] MultBulletDir = { new Vector2(0f, 1) };//子弹射击出现的方向
-    //List<Vector2> MultBulletPos = new List<Vector2>();//子弹射击出现的位置
-    //List<Vector2> MultBulletDir = new List<Vector2>();
-    //int dirCount = 3;//最少3个方向
-    //float ShootRadius = 3;//发射圈半径
-    //public bool isTurnToTarget = false;//是否会自动转向我方飞机方向
+    public GameObject BulletPrefab;//子弹预设
+    float ShootRadius = 0.5f;//发射圈半径
+    int dirCount = 3;//最少3个方向
 
     //// Use this for initialization
-    //public void Start()
-    //{
-    //    //base.Start();
-    //    switch (GlobalData.RankLevel)
-    //    {
-    //        case GlobalData.GameRank.Easy:
-    //            break;
-    //        case GlobalData.GameRank.Normal:
-    //            dirCount = 5;
-    //            break;
-    //        case GlobalData.GameRank.Hard:
-    //            dirCount = 7;
-    //            break;
-    //        case GlobalData.GameRank.Lunatic:
-    //            dirCount = 9;
-    //            break;
-    //    }
-    //    for (int i = 0; i < dirCount; i++)
-    //    {
-    //        float clipAngle = 2 * Mathf.PI / dirCount;
-    //        float x = Mathf.Cos(i * clipAngle);
-    //        float y = Mathf.Sin(i * clipAngle);
-    //        Vector2 dir = new Vector2(x, y);
-    //        Vector2 pos = new Vector2(x * ShootRadius * 10, y * ShootRadius * 10);
+    public void Start()
+    {
+        //base.Start();
+        switch (GlobalData.RankLevel)
+        {
+            case GlobalData.GameRank.Easy:
+                break;
+            case GlobalData.GameRank.Normal:
+                dirCount = 5;
+                break;
+            case GlobalData.GameRank.Hard:
+                dirCount = 7;
+                break;
+            case GlobalData.GameRank.Lunatic:
+                dirCount = 9;
+                break;
+        }
+        setBulletSortingLayer(CommandString.EnemyBulletLayer);
+        //hitEnable = false;
+        //Bullet_dirSameSpeed = true;
+    }
+    protected override void InitBullet()
+    {
+        base.InitBullet();
+        for (int i = 0; i < dirCount; i++)
+        {
+            float clipAngle =2* Mathf.PI / dirCount;
+            float offAngle = GetAngleByTarget(MyPlane.MyPos);
+            float x = Mathf.Cos(i * clipAngle + offAngle);
+            float y = Mathf.Sin(i * clipAngle + offAngle);
+            
+            Vector2 dir = new Vector2(x, y).normalized;
+            Vector2 pos = dir * ShootRadius + (Vector2)transform.position;
+            InitOneBullet(pos, dir);
+        }
+        
+    }
 
-    //        MultBulletDir.Add(dir);
-    //        MultBulletPos.Add(pos);
-    //    }
-    //    //bulletName = "bullet4_0";
-    //    //bulletName = obj_OwnBullet.name;
-    //    //shootRate = AudioManager.EveryBeatsLenght;
-    //    setBulletSortingLayer(CommandString.EnemyBulletLayer);
-    //    hitEnable = false;
-    //    //dir_Bullets = MultBulletDir.ToArray();
+    /// <summary>
+    /// 发射一个子弹
+    /// </summary>
+    /// <param name="pos">出现位置</param>
+    /// <param name="dir">方向</param>
+    void InitOneBullet(Vector2 pos,Vector2 dir) {
+        GameObject bullet = Instantiate(BulletPrefab) as GameObject;
+        bullet.transform.position = pos;
+        bullet.transform.parent = UIEmitterRoot.tra_ShootRoot;
+        bullet.transform.localScale = Vector3.one;
 
-    //    //pos_Bullets = MultBulletPos.ToArray();
-    //    Bullet_dirSameSpeed = true;
-    //}
+        BulletBase_Touhou bulletBase = bullet.GetComponent<BulletBase_Touhou>();
+        bulletBase.renderer.sortingLayerName = bulletLayerName;
+        bulletBase.RotationWithDirction(dir);
+        bulletBase.rigidbody2D.velocity = dir * SpeedScale;
 
-    ////// Update is called once per frame
-    //public void Update()
-    //{
-    //    //base.Update();
-    //    if (isTurnToTarget)
-    //    {
-    //        List<Vector2> dir_temp = new List<Vector2>();
-    //        List<Vector2> pos_temp = new List<Vector2>();
-    //        for (int i = 0; i < dirCount; i++)
-    //        {
-    //            float clipAngle = 2 * Mathf.PI / dirCount;
-    //            float offAngle = GetAngleByTarget((Vector2)MyPlane.MyPos);
-    //            float x = Mathf.Cos(i * clipAngle + offAngle);
-    //            float y = Mathf.Sin(i * clipAngle + offAngle);
-    //            Vector2 dir = new Vector2(x, y);
-    //            Vector2 pos = new Vector2(x * ShootRadius * 10, y * ShootRadius * 10);
+        //生产发子弹的特效
+        GameObject effect = GameObject.Instantiate(Resources.Load(CommandString.BulletPrefabPath + "ShootBulletEffect")) as GameObject;
+        effect.transform.parent = UIEmitterRoot.tra_ShootRoot;
+        effect.transform.position = bullet.transform.position;
+        effect.transform.localScale = Vector3.one * 2;
+    }
+    protected override void Shoot()
+    {
+        base.Shoot();
+        if (master != null)
+        {
+            if (shootSpace > 0)//如果有射击时间间隔
+            {
+                if (Time.time > nextShootSpace)
+                {
+                    //nextShootSpace = Time.time + shootSpace;
+                    //进来以后让持续射击时间开始增加
+                    nextShootDuration += Time.deltaTime;
+                    if (nextShootDuration < shootDuration)
+                    {
+                        ShootBulletRate();
+                    }
+                    else
+                    {
+                        nextShootDuration = 0;
+                        nextShootSpace = Time.time + shootSpace;
+                    }
+                }
+            }
+            else
+            {
+                ShootBulletRate();
+            }
+        }
+        else
+        {
+            Debug.LogError("shooter master = null");
+        }
+    }
 
-    //            dir_temp.Add(dir);
-    //            pos_temp.Add(pos);
-    //        }
-    //        MultBulletDir = dir_temp;
-    //        MultBulletPos = pos_temp;
-    //        //dir_Bullets = MultBulletDir.ToArray();
+    //射出子弹频率
+    protected override void ShootBulletRate()
+    {
+        if (Time.time > nextShoot)
+        {
+            nextShoot = Time.time + shootRate;
+            if (CanShootBulletCount == 0)
+            { //子弹打完了
+                return;
+            }
+            if (CanShootBulletCount <= -1)
+            { //机体无限子弹
+                InitBullet();
+            }
+            else if (CanShootBulletCount > 0)
+            {
+                InitBullet();
+                CanShootBulletCount--;
+            }
+            if (shootSound != null)
+            {
+                AudioManager.AddBulletSound(shootSound);
+            }
 
-    //        //pos_Bullets = MultBulletPos.ToArray();
-    //    }
-    //}
+        }
+        //AudioPlay();
+    }
+
+    public override void Update()
+    {
+        base.Update();
+        Shoot();
+    }
 }
