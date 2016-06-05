0f1921e77d4ed0399308f8435bab121cd2c90da1
sdqq1234 <zhangsdqq1234@foxmail.com>
2016-06-05 22:53:47 +0900

    new branch test
    branch1 test

---
+++ Assets/Script/Emitter/Touhou/FlowerShooter.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlowerShooter : EmitterBase
+{
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
